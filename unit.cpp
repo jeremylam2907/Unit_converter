f63b068626bbbcdada3aa0c0fadb16273c32166b
jeremylam2907 <59300941+jeremylam2907@users.noreply.github.com>
2020-05-30 14:32:50 -0500

    update

--- unit.cpp
+++ unit.cpp
@@ -189,192 +189,192 @@ void Convert()
     else if (UserInput ==2)
     {
         cout << "[1] for length   \n[2] for area   \n[3] for volume\n";
-    cout << "[4] for mass   \n[5] for pressure\n[6] for velocity \n[7] for temperature\n";    
-    int CustomarySelect;
-    cin >> CustomarySelect;
-    cout << endl;
-
-
-    if (CustomarySelect == 1) // length
-    {
-        cout << "You are converting from:\n [1] inches\n [2] feet\n [3] yards\n [4] miles\n\n";
-        double Length;
-        cin >> Length;
-        double a;
-        
-        if( Length == 1)
-        { 
-            cout << "Enter in inches: "; cin >> a;
-            double b = a;
-            cout << "Equals to: " << b/39.37 << " meters";
-        }
-        else if (Length == 2)
-        {
-            cout << "Enter in feet: "; cin >> a;
-            double b = a * 12;
-            cout << "Equals to: " << b/39.37 << " meters";
-        }
-        else if (Length == 3)
-        {
-            cout << "Enter in yard: "; cin >> a;
-            double b = a * 36;
-            cout << "Equals to: " << b/39.37 << " meters";
-        }
-        else
-        {
-            cout << "Enter in miles: "; cin >> a;
-            double b = a * 63360;
-            cout << "Equals to: " << b/39.37 << " meters";
-        }
-        cout << "\nPREFIX REFERENCE:\n\nmega: e6\nkilo: e3\ncenti: e-2\nmilli: e-3\nmicro: e-6\nnano: e-9\n\n";
-    }
-    else if (CustomarySelect == 2) // Area
-    {
-        cout << "You are converting from:\n [1] square inches\n [2] square feet\n [3] square miles\n\n";
-        int Area;
-        cin >> Area;
-        double c;
-        
-        if( Area == 1)
-        { 
-            cout << "Enter in square inches: "; cin >> c;
-            double d = c;
-            cout << "Equals to: " << d/1550 << " square meters";
-        }
-        else if ( Area == 2)
-        {
-            cout << "Enter in square feet: "; cin >> c;
-            double d = c * 144;
-            cout << "Equals to: " << d/1550 << " square meters";
-        }
-        else if ( Area == 3)
-        {
-            cout << "Enter in square miles: "; cin >> c;
-            double d = c * 4.014e+9;
-            cout << "Equals to: " << d/1550 << " square meters";
-        }
-
-        cout << "\nPREFIX REFERENCE:\n\nkilo: e6\ncenti: e-4\n\n";
-    }
-    else if (CustomarySelect == 3) // Volume
-    {
-        cout << "You are converting from:\n [1] floz\n [2] cups\n [3] pints\n [4] quarts\n [5] gallons\n\n";
-        double Vol;
-        cin >> Vol;
-        double a;
-
-        //Conversion
-        if( Vol == 1)
-        { 
-            cout << "Enter in floz: "; cin >> a;
-            double b = a;
-            cout << "Equals to: " << b/33814 << " cubic meters";
-        }
-        else if( Vol == 2)
-        { 
-            cout << "Enter in cups: "; cin >> a;
-            double b = a*8.115;
-            cout << "Equals to: " << b/33814 << " cubic meters";
-        }
-        else if( Vol == 3)
-        { 
-            cout << "Enter in pints: "; cin >> a;
-            double b = a*16;
-            cout << "Equals to: " << b/33814 << " cubic meters";
-        }
-        else if( Vol == 4)
-        { 
-            cout << "Enter in quarts: "; cin >> a;
-            double b = a*32;
-            cout << "Equals to: " << b/33814 << " cubic meters";
-        }
-        else if( Vol == 5)
-        { 
-            cout << "Enter in gallons: "; cin >> a;
-            double b = a*128;
-            cout << "Equals to: " << b/33814 << " cubic meters";
-        }
-            cout << "\nPREFIX REFERENCE:\n\nkilo: e9\ncenti: e-6\n1 cubic meter = 1000 liter\n";
-    }
-    else if (CustomarySelect == 4) // Mass
-    {
-        cout << "You are converting from:\n [1] pounds\n [2] ounce\n\n";
-        int Mass;
-        cin >> Mass;
-        double c;
-
-        //Conversion
-        if( Mass == 1)
-        { 
-            cout << "Enter in pounds: "; cin >> c;
-            double d = c * 16; //convert to ounce
-            cout << "Equals to: " << d/35.274 << " Kilograms";
-        }
-        else if ( Mass == 2)
-        {
-            cout << "Enter in ounces: ", cin >> c;
-            cout << "Equals to: " << c/35.274 << " Kilograms";
-        }
-        cout << "\nPREFIX REFERENCE:\n\n metric ton: e3\n grams: e-3\n milligrams: e6\n";
-    }
-    else if (CustomarySelect == 5) // Pressure
-    {
-        double a;
-        cout << "You are converting from PSI to Pa\n" << "Enter in PSI: "; cin >> a;
+        cout << "[4] for mass   \n[5] for pressure\n[6] for velocity \n[7] for temperature\n";    
+        int CustomarySelect;
+        cin >> CustomarySelect;
         cout << endl;
-        cout << "Equal to: " << a * 6894.76 << " Pascal";
-        cout << "\n\n 1 Standard Atmosphere = 101325 Pascal ";
-    }
-    else if (CustomarySelect == 6) // Velocity
-    {
-        cout << "You are converting from:\n [1] ft/sec\n [2] mi/h";
-        int Velo;
-        cin >> Velo;
-        cout << "You are converting to: \n [1] m/s\n [2] km/h";
-        int VeloOut;
-        cin >> VeloOut;
-        double a;
-        
-        //Conversion
-        if( Velo == 1)
+
+
+        if (CustomarySelect == 1) // length
         {
-            cout << "Enter in ft/sec: "; cin >> a;
-            if (VeloOut == 1)
+            cout << "You are converting from:\n [1] inches\n [2] feet\n [3] yards\n [4] miles\n\n";
+            double Length;
+            cin >> Length;
+            double a;
+            
+            if( Length == 1)
+            { 
+                cout << "Enter in inches: "; cin >> a;
+                double b = a;
+                cout << "Equals to: " << b/39.37 << " meters";
+            }
+            else if (Length == 2)
             {
-                cout << "Equals: " << a * 0.3048 << "m/s";
+                cout << "Enter in feet: "; cin >> a;
+                double b = a * 12;
+                cout << "Equals to: " << b/39.37 << " meters";
+            }
+            else if (Length == 3)
+            {
+                cout << "Enter in yard: "; cin >> a;
+                double b = a * 36;
+                cout << "Equals to: " << b/39.37 << " meters";
             }
             else
             {
-                cout << "Equals: " << a * 1.09728 << "km/h";
+                cout << "Enter in miles: "; cin >> a;
+                double b = a * 63360;
+                cout << "Equals to: " << b/39.37 << " meters";
             }
+            cout << "\nPREFIX REFERENCE:\n\nmega: e6\nkilo: e3\ncenti: e-2\nmilli: e-3\nmicro: e-6\nnano: e-9\n\n";
         }
-        else if( Velo == 2)
+        else if (CustomarySelect == 2) // Area
         {
-            cout << "Enter in mi/h: "; cin >> a;
-            if (VeloOut == 1)
-            {
-                cout << "Equals: " << a * 0.44704 << "m/s";
+            cout << "You are converting from:\n [1] square inches\n [2] square feet\n [3] square miles\n\n";
+            int Area;
+            cin >> Area;
+            double c;
+            
+            if( Area == 1)
+            { 
+                cout << "Enter in square inches: "; cin >> c;
+                double d = c;
+                cout << "Equals to: " << d/1550 << " square meters";
             }
-            else
+            else if ( Area == 2)
             {
-                cout << "Equals: " << a * 1.60934 << "km/h";
+                cout << "Enter in square feet: "; cin >> c;
+                double d = c * 144;
+                cout << "Equals to: " << d/1550 << " square meters";
             }
+            else if ( Area == 3)
+            {
+                cout << "Enter in square miles: "; cin >> c;
+                double d = c * 4.014e+9;
+                cout << "Equals to: " << d/1550 << " square meters";
+            }
+
+            cout << "\nPREFIX REFERENCE:\n\nkilo: e6\ncenti: e-4\n\n";
         }
-    }
-    else if (CustomarySelect == 7) // Temperature
+        else if (CustomarySelect == 3) // Volume
+        {
+            cout << "You are converting from:\n [1] floz\n [2] cups\n [3] pints\n [4] quarts\n [5] gallons\n\n";
+            double Vol;
+            cin >> Vol;
+            double a;
+
+            //Conversion
+            if( Vol == 1)
+            { 
+                cout << "Enter in floz: "; cin >> a;
+                double b = a;
+                cout << "Equals to: " << b/33814 << " cubic meters";
+            }
+            else if( Vol == 2)
+            { 
+                cout << "Enter in cups: "; cin >> a;
+                double b = a*8.115;
+                cout << "Equals to: " << b/33814 << " cubic meters";
+            }
+            else if( Vol == 3)
+            { 
+                cout << "Enter in pints: "; cin >> a;
+                double b = a*16;
+                cout << "Equals to: " << b/33814 << " cubic meters";
+            }
+            else if( Vol == 4)
+            { 
+                cout << "Enter in quarts: "; cin >> a;
+                double b = a*32;
+                cout << "Equals to: " << b/33814 << " cubic meters";
+            }
+            else if( Vol == 5)
+            { 
+                cout << "Enter in gallons: "; cin >> a;
+                double b = a*128;
+                cout << "Equals to: " << b/33814 << " cubic meters";
+            }
+                cout << "\nPREFIX REFERENCE:\n\nkilo: e9\ncenti: e-6\n1 cubic meter = 1000 liter\n";
+        }
+        else if (CustomarySelect == 4) // Mass
+        {
+            cout << "You are converting from:\n [1] pounds\n [2] ounce\n\n";
+            int Mass;
+            cin >> Mass;
+            double c;
+
+            //Conversion
+            if( Mass == 1)
+            { 
+                cout << "Enter in pounds: "; cin >> c;
+                double d = c * 16; //convert to ounce
+                cout << "Equals to: " << d/35.274 << " Kilograms";
+            }
+            else if ( Mass == 2)
+            {
+                cout << "Enter in ounces: ", cin >> c;
+                cout << "Equals to: " << c/35.274 << " Kilograms";
+            }
+            cout << "\nPREFIX REFERENCE:\n\n metric ton: e3\n grams: e-3\n milligrams: e6\n";
+        }
+        else if (CustomarySelect == 5) // Pressure
         {
             double a;
-            cout << "Input in degree Farhenheit: "; cin >> a;
-            cout << "Convert to:\n [1] degree Celcius\n [2] Kelvin\n";
-            int Degree;
-            cin >> Degree;
-            if(Degree == 1)
+            cout << "You are converting from PSI to Pa\n" << "Enter in PSI: "; cin >> a;
+            cout << endl;
+            cout << "Equal to: " << a * 6894.76 << " Pascal";
+            cout << "\n\n 1 Standard Atmosphere = 101325 Pascal ";
+        }
+        else if (CustomarySelect == 6) // Velocity
+        {
+            cout << "You are converting from:\n [1] ft/sec\n [2] mi/h";
+            int Velo;
+            cin >> Velo;
+            cout << "You are converting to: \n [1] m/s\n [2] km/h";
+            int VeloOut;
+            cin >> VeloOut;
+            double a;
+            
+            //Conversion
+            if( Velo == 1)
             {
-                cout << (a - 32) * 5/9 ;
+                cout << "Enter in ft/sec: "; cin >> a;
+                if (VeloOut == 1)
+                {
+                    cout << "Equals: " << a * 0.3048 << "m/s";
+                }
+                else
+                {
+                    cout << "Equals: " << a * 1.09728 << "km/h";
+                }
             }
-            else if(Degree==2)
+            else if( Velo == 2)
             {
-                cout << (a - 32) * 5/9 + 273.15;
+                cout << "Enter in mi/h: "; cin >> a;
+                if (VeloOut == 1)
+                {
+                    cout << "Equals: " << a * 0.44704 << "m/s";
+                }
+                else
+                {
+                    cout << "Equals: " << a * 1.60934 << "km/h";
+                }
+            }
+        }
+        else if (CustomarySelect == 7) // Temperature
+            {
+                double a;
+                cout << "Input in degree Farhenheit: "; cin >> a;
+                cout << "Convert to:\n [1] degree Celcius\n [2] Kelvin\n";
+                int Degree;
+                cin >> Degree;
+                if(Degree == 1)
+                {
+                    cout << (a - 32) * 5/9 ;
+                }
+                else if(Degree==2)
+                {
+                    cout << (a - 32) * 5/9 + 273.15;
             }    
         }
     }
